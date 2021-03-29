716ee024f03f4adbd25d90993bc9215dc027139c
GrenAkaike <gren.akaike@gmx.de>
2021-03-29 14:04:05 +0200

    Decorator fix

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -1,6 +1,6 @@
 module Spree
   module OrderMailerDecorator
-    helper Spree::BaseHelper
+    include Spree::BaseHelper
   end
 end
 
--- app/mailers/spree/shipment_mailer_decorator.rb
+++ app/mailers/spree/shipment_mailer_decorator.rb
@@ -1,6 +1,6 @@
 module Spree
   module ShipmentMailerDecorator
-    helper Spree::BaseHelper
+    include Spree::BaseHelper
   end
 end
 
